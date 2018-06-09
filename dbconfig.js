16c3f014979bfe45cf67b6524b98e1584aee309d
Albin B <Baawa@users.noreply.github.com>
2018-06-09 13:42:34 +0200

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -3,7 +3,7 @@ module.exports = {
     accessKey           : 'oZCq3CqRuK24Bt2txchdOuRHyw36eJ6hf1G1QENi'
   },
   db: {
-    database            : process.env.RDS_DB_NAME  || 'dat076_db',
+    database            : process.env.RDS_DB_NAME  || 'soapcompany',
     host                : process.env.RDS_HOSTNAME || 'localhost',
     port                : process.env.RDS_PORT     || 3306,
     user                : process.env.RDS_USERNAME || 'root',
